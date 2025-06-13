902fb60684567b16db6cba12d12e0f7f6e01cbfc
Gaál Sándor <gmac4247@gmail.com>
2025-06-13 14:42:51 +0200

    Update ExampleUsage.cs
    Added the term "Exact" to the keys to avoid conflict

--- ExampleUsage.cs
+++ ExampleUsage.cs
@@ -7,9 +7,9 @@ class Program
         double r = 2;
         double h = 5;
 
-        Console.WriteLine(Geometry.AreaOfCircle(r));            // 12.8
-        Console.WriteLine(Geometry.CircumferenceOfCircle(r));   // 12.8
-        Console.WriteLine(Geometry.VolumeOfSphere(r));          // ≈28.84
-        Console.WriteLine(Geometry.VolumeOfCone(r, h));         // ≈11.31
+        Console.WriteLine(Geometry.ExactAreaOfCircle(r));            // 12.8
+        Console.WriteLine(Geometry.ExactCircumferenceOfCircle(r));   // 12.8
+        Console.WriteLine(Geometry.ExactVolumeOfSphere(r));          // ≈28.84
+        Console.WriteLine(Geometry.ExactVolumeOfCone(r, h));         // ≈11.31
     }
 }
