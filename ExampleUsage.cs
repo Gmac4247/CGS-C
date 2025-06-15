4944c16e2030648ccb4c1ccb669297a15f2a24a9
Gaál Sándor <gmac4247@gmail.com>
2025-06-15 18:41:04 +0200

    Update ExampleUsage.cs

--- ExampleUsage.cs
+++ ExampleUsage.cs
@@ -4,12 +4,10 @@ class Program
 {
     static void Main()
     {
-        double r = 2;
-        double h = 5;
 
-        Console.WriteLine(Geometry.ExactAreaOfCircle(r));            // 12.8
-        Console.WriteLine(Geometry.ExactCircumferenceOfCircle(r));   // 12.8
-        Console.WriteLine(Geometry.ExactVolumeOfSphere(r));          // ≈28.84
-        Console.WriteLine(Geometry.ExactVolumeOfCone(r, h));         // ≈11.31
+        Console.WriteLine(BasicGeometry.ExactCircleArea(r));            // 12.8
+        Console.WriteLine(BasicGeometry.ExactCircumference(r));   // 12.8
+        Console.WriteLine(BasicGeometry.ExactSphereVolume(r));          // ≈28.84
+        Console.WriteLine(BasicGeometry.ExactConeVolume(r, h));         // ≈11.31
     }
 }
