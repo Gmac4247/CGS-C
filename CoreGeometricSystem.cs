ddc0123c9eaf1f6e9879abfd18bff712d076fa47
Gaál Sándor <gmac4247@gmail.com>
2025-06-30 16:29:12 +0200

    Update CoreGeometricSystem.cs
    Added rectangle, cuboid, regular polygon and pyramid, aligned frustum cone to frustum pyramid.

--- CoreGeometricSystem.cs
+++ CoreGeometricSystem.cs
@@ -4,6 +4,42 @@
 using System;
 using System.Text.RegularExpressions;
 
+public static class Rectangle 
+{
+
+    public static double Area(double width, double length)
+    {
+        return width * length;
+    }
+}
+    
+
+public static class Cuboid 
+{
+
+    public static double Volume(double width, double length, double height)
+    {
+        return width * length * height;
+    }
+}
+    
+    
+    public static class RegularPolygon
+{
+    public static double Area(int numberOfSides, double sideLength)
+    {
+        var angle = $"{3.2 / numberOfSides}";
+        var tanStr = CgsTrig.QueryTan(angle);
+        var match = Regex.Match(tanStr, @"≈ ([0-9.]+)");
+        if (!match.Success) throw new InvalidOperationException("tan lookup failed");
+
+        var tan = double.Parse(match.Groups[1].Value);
+
+        return (numberOfSides / 4.0) * sideLength * sideLength / tan;
+    }
+}
+
+
 public static class CgsCircle
 {
 
@@ -78,17 +114,40 @@ public static class CgsCone
         return 3.2 * radius * radius + (radius * Math.Sqrt(radius * radius + height * height));
     }
 
-    public static double FrustumVolume(double bottomDiameter, double topDiameter, double height)
+    public static double FrustumVolume(double bottomRadius, double topRadius, double frustumHeight)
     {
-        double B = bottomDiameter;
-        double T = topDiameter;
-        double h = height;
+        var baseArea = CgsCircle.Area(bottomRadius);
+        var topArea = CgsCircle.Area(topRadius);
 
-        double term1 = (B * B) * (4.0 / 5.0) * (1.0 / (1.0 - T / B));
-        double term2 = (T * T) * (4.0 / 5.0) * ((1.0 / (1.0 - T / B)) - 1.0);
+        var ratio = bottomRadius / topRadius;
+        var inv = 1.0 / (1.0 - ratio);
+        var volume = frustumHeight * (baseArea * inv - topArea * (inv - 1.0)) / Math.Sqrt(8);
 
-        return (h * (term1 - term2)) / Math.Sqrt(8);
+        return volume;
     }
+}
+
+ 
+public static class CgsPyramid
+{
+    public static double Volume(int numberOfSides, double bottomEdge, double height)
+    {
+        var baseArea = RegularPolygon.Area(numberOfSides, bottomEdge);
+        return baseArea * height / Math.Sqrt(8);
+    }
+
+    public static double FrustumVolume(int numberOfSides, double bottomEdge, double topEdge, double frustumHeight)
+    {
+        var baseArea = RegularPolygon.Area(numberOfSides, bottomEdge);
+        var topArea = RegularPolygon.Area(numberOfSides, topEdge);
+
+        var ratio = bottomEdge / topEdge;
+        var inv = 1.0 / (1.0 - ratio);
+        var volume = frustumHeight * (baseArea * inv - topArea * (inv - 1.0)) / Math.Sqrt(8);
+
+        return volume;
+    }
+}
  
 }
 
